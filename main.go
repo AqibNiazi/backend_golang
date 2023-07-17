795e0ae0767683a535da737cf18333de7678ef00
AqibNiazi <aqibjaved5201@gmail.com>
2023-07-17 18:54:07 +0500

    adding routes

--- main.go
+++ main.go
@@ -6,12 +6,28 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", pathhandler)
+	// http.HandleFunc("/contact", contactHandler)
+
 	fmt.Println("Starting the server on port:3000")
 	http.ListenAndServe(":3000", nil)
-
 }
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	fmt.Fprintf(w, "<h1>This is heading 2</h1>")
+}
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	fmt.Fprintf(w, "<h2>This is contact page</h2>")
+}
+func pathhandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		//
+	}
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello and welcome to course</h1>")
 }
